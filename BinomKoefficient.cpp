26843872b79664f07bdc5ea5349f485d023a386c
VladimirMironov <44780782+vladimirVFX@users.noreply.github.com>
2020-04-13 17:56:00 +0300

    InitialCommit

---
+++ BinomKoefficient.cpp
@@ -0,0 +1,27 @@
+﻿// BinomKoefficient.cpp
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    system("chcp 1251 > null");
+    const int n = 10;
+    int bnm[n + 1];
+    int k = 0;
+
+    bnm[0] = 1;
+    cout << bnm[0];
+
+    while (k < n) {
+        bnm[k + 1] = bnm[k] * (n - k) / (k + 1);
+        cout << " " << bnm[k + 1];
+        k++;
+    }
+
+    cout << endl;
+    system("pause > null");
+    return 0;
+}
